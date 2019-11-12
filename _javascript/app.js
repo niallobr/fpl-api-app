df130064b38d615593bd9250bab9caf3f8c1f4b1
niallobr <204252+niallobr@users.noreply.github.com>
2019-11-12 22:37:23 +0000

    Adding eslint and relevant fixes

--- _javascript/app.js
+++ _javascript/app.js
@@ -1,19 +1,17 @@
-fetch('http://127.0.0.1:8888/proxy')
-.then(response => response.json())
-.then(data => {
+fetch('http://localhost:8888/proxy/api/bootstrap-static/')
+    .then((response) => response.json())
+    .then((data) => {
+      const fpl = data;
+      const teamDiv = document.querySelector('#teamList');
+      const teams = [];
+      const numTeams = fpl.teams.length;
 
-  const fpl = data;
-  const teamDiv = document.querySelector('#teamList');
-  const teams = [];
-  const numTeams = fpl.teams.length;
+      console.log(fpl);
 
-  console.log(fpl);
+      for (i = 0; i < numTeams; i++) {
+        teams.push(fpl.teams[i].name);
+      }
 
-  for (i = 0;i < numTeams;i++) {
-    teams.push(fpl.teams[i].name);
-  }
-
-  teams.forEach((teamName) => teamDiv.innerHTML += `<li>${teamName}</li>`);
-
-})
-.catch(error => console.error(error));
+      teams.forEach((teamName) => teamDiv.innerHTML += `<li>${teamName}</li>`);
+    })
+    .catch((error) => console.error(error));
