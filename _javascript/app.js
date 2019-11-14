ecd1ff0e1db19e38e0c45dce38788aa162adf83d
niallobr <204252+niallobr@users.noreply.github.com>
2019-11-14 13:38:49 +0000

    Adding dropdown functionlity. Making small accordion fix

--- _javascript/app.js
+++ _javascript/app.js
@@ -1,5 +1,3 @@
-const accordion = new Accordion('.accordion-container');
-
 fetch('http://localhost:8888/proxy/api/bootstrap-static/')
     .then((response) => response.json())
     .then((data) => {
@@ -7,8 +5,8 @@ fetch('http://localhost:8888/proxy/api/bootstrap-static/')
       const teamDiv = document.querySelector('.teamsList');
       const teams = [];
       const numTeams = fpl.teams.length;
-      const team1 = "<div class='ac'><h2 class='ac-q' tabindex='0'>";
-      const team2 = "</h2><div class='ac-a'><p>Some content<br>more content<br>even more</p></div></div>";
+      const team1 = '<div class=\'ac\'><h2 class=\'ac-q\' tabindex=\'0\'>';
+      const team2 = '</h2><div class=\'ac-a\'><p>Some content<br>more content<br>even more</p></div></div>';
 
       console.log(fpl);
 
@@ -20,7 +18,15 @@ fetch('http://localhost:8888/proxy/api/bootstrap-static/')
         teamDiv.innerHTML += team1 + `${teamName}` + team2;
       });
 
-      const teamsAccordion = new Accordion('.teamsList');
+      new Accordion('.teamsList');
 
+      const teamsDown = document.querySelector('.teams-dropdown');
+      teamsDown.addEventListener('click', (makeActive) => {
+        if (teamsDown.classList.length < 3) {
+          teamsDown.classList.add('is-active');
+        } else {
+          teamsDown.classList.remove('is-active');
+        }
+      });
     })
     .catch((error) => console.error(error));
